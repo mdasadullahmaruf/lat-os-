5d3a568288190838bacf666e0bd3da3883dd154c
MD Asad Ullah <mdasadullahmaruf1@gmail.com>
2026-06-24 17:58:01 +0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,2 @@
 rootProject.name = "Lat OS"
 include(":app")
-
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "Lat OS"
-include(":app")
