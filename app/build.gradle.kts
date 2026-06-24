cb3e19d509b1d77d7845e2162f0c0bcd79eed43e
MD Asad Ullah <mdasadullahmaruf1@gmail.com>
2026-06-24 18:16:59 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.mdasadullahmaruf.latos"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mdasadullahmaruf.latos"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
